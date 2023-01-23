50354174eae35e1d0f86fe1137b33f8c040917c6
Adam Kumm <adamk@fitspo.app>
2023-01-23 11:47:02 -0700

    Made it so we can login and navigate to the home screen on mobile. Still need to properly set up Navigators based on auth token.

--- packages/app/navigation/native/index.tsx
+++ packages/app/navigation/native/index.tsx
@@ -1,7 +1,10 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { HomeScreen } from 'app/features/home/screen'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeScreen } from 'app/features/home/home-screen'
 import { LoginScreen } from 'app/features/login/login-screen'
 import { SignupScreen } from 'app/features/signup/signup-screen'
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 export function NativeNavigation() {
 
@@ -11,9 +14,22 @@ export function NativeNavigation() {
       <AuthStack.Navigator>
         <AuthStack.Screen name="login" component={LoginScreen} options={{ headerShown: false, }}/>
         <AuthStack.Screen name="signup" component={SignupScreen} options={{ title: 'Signup', }} />
+        <AuthStack.Screen name="home" component={HomeScreen} options={{ title: 'Home', tabBarBadge: undefined, 
+          tabBarIcon: ({ focused, color, size }) => (<Icon name={focused ? 'home' : 'home-outline'}  size={25}/>) }} />
       </AuthStack.Navigator>
     );
   }
+
+  const HomeTabStack = createBottomTabNavigator();
+  function HomeTabStackScreen() {
+    return (
+      <HomeTabStack.Navigator>
+        <HomeTabStack.Screen name="home" component={HomeScreen} options={{ title: 'Home', tabBarBadge: undefined, 
+          tabBarIcon: ({ focused, color, size }) => (<Icon name={focused ? 'home' : 'home-outline'}  size={25}/>) }} />
+      </HomeTabStack.Navigator>
+    );
+  }
+
 
   return (
     AuthStackScreens()
