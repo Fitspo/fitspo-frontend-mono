a72050e37add35b814208a65554360d7d17ad75d
Adam Kumm <adamk@fitspo.app>
2023-01-22 17:02:23 -0700

    Fix vercel build errors because of linking.

--- packages/app/provider/navigation/index.tsx
+++ packages/app/provider/navigation/index.tsx
@@ -1,38 +1,28 @@
 import { NavigationContainer } from '@react-navigation/native'
 import * as Linking from 'expo-linking'
 import { useMemo } from 'react'
-import {navigationRef} from '../../navigation/native/NavigationService';
 
 export function NavigationProvider({
   children,
 }: {
   children: React.ReactNode
 }) {
-  let screens = {
-    login: 'login',
-    signup: 'signup',
-    home: 'home',
-    createPost: 'createPost',
-    profile: 'profile',
-    'user-detail': 'user/:id',
-    settings: 'settings'
-  }
   return (
-      <NavigationContainer
-        linking={useMemo(
-          () => ({
-            prefixes: [Linking.createURL('/')],
-            config: {
-              initialRouteName: 'login',
-              screens: screens,
+    <NavigationContainer
+      linking={useMemo(
+        () => ({
+          prefixes: [Linking.createURL('/')],
+          config: {
+            initialRouteName: 'login',
+            screens: {
+              login: 'login',
             },
-          }),
-          []
-        )}
-        ref={navigationRef}
-        onStateChange={state => console.log('New state is', state)}
-      >
-        {children}
-      </NavigationContainer>
+          },
+        }),
+        []
+      )}
+    >
+      {children}
+    </NavigationContainer>
   )
 }
