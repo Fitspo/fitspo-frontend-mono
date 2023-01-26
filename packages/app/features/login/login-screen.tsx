8c701f5fc99cca799ecd26954904cc6cdfd8dc51
Adam Kumm <adamk@fitspo.app>
2023-01-26 12:49:02 -0700

    Making sure the user is stored on mobile when we login

--- packages/app/features/login/login-screen.tsx
+++ packages/app/features/login/login-screen.tsx
@@ -11,6 +11,7 @@ import { SIGN_IN } from './gql';
 import * as Auth from '../../helpers/auth'
 import * as NavigationService from '../../navigation/native/NavigationService';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SecureStore from 'expo-secure-store';
 
 const VALID_EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 const VALID_PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
@@ -103,7 +104,7 @@ export function LoginScreen() {
 
                 // Set the UserContext
                 AsyncStorage.setItem('user', JSON.stringify(data.signIn.user))
-
+                SecureStore.setItemAsync('user', JSON.stringify(data.signIn.user))
                 // clear forms and reset data
                 reset()
                 setEmail('')
